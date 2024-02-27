32c131aa1b466a94c746aa92a0578c6eb3245aaf
Renato Rojas <roj23813@uvg.edu.gt>
2024-02-27 17:24:49 -0600

    Se implementó el manejo de la pila en forma de ArrayList

--- ImpArrayList.java
+++ ImpArrayList.java
@@ -3,9 +3,33 @@ import java.util.ArrayList;
 /**
  * ImpArrayList
  */
-public class ImpArrayList {
+public class ImpArrayList <T> implements StackInterface <T> {
 
-    ArrayList <Integer> arrayList = new ArrayList<>();
+    ArrayList <T> arrayList1 = new ArrayList<>();
     private Calculadora calculator = Calculadora.getInstance();
+
+
+    @Override
+    public void push(T value) {
+        arrayList1.add(value);
+        }
+    
+    @Override
+    public T pop() {
+        T value = arrayList1.get(arrayList1.size()-1);
+        arrayList1.remove(arrayList1.size()-1);
+        return value;
+    }
+
+    @Override
+    public int size() {
+        return arrayList1.size();
+    }
+    @Override
+    public boolean isEmpty() {
+        return arrayList1.isEmpty();
+    }
+
+
 
 }
