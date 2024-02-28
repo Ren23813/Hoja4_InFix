6a71edad871c4777ce68c7a06f5e688741a9cac4
Renato Rojas <roj23813@uvg.edu.gt>
2024-02-27 22:03:42 -0600

    actualizado clase de lista doble

--- ListaDoble.java
+++ ListaDoble.java
@@ -5,12 +5,22 @@ public class ListaDoble <T> {
     Node<T> firstNode;
 
 
-    public void push(T value) {
+    public void push(T value, int index) {
         if ( current== null) { // En caso aún no se tenga registro de algún nodo.
             current= new Node<T>(value);
             current.constructDoble(value);
             firstNode = current;
-        } else { // Para crear un nuevo nodo, cuando existe uno previo.
+        } else { // Para crear un nuevo nodo, cuando ya existe uno previo.
+            if (index != size()){
+                Node<T> iterator = firstNode;
+                int posicion = size() - index;
+                for (int i = 0; i < posicion-1; i++) {
+                    iterator = iterator.getNext();
+                }
+                current = new Node<T>(value);
+                current.setPrevious(iterator);
+                current.setNext(iterator.getNext());
+            }
             next = new Node<T>(value);
             current.setNext(next);
             prev = current;
